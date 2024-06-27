56354216be52f3a3b7b7e23d837161149c6bc883
Pawan Singh <113767530+almightycoder4@users.noreply.github.com>
2024-06-28 02:33:11 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from ultralytics import YOLO
 
 def create_app():
     app = Flask(__name__)
     from .api import ocr_bp
     app.register_blueprint(ocr_bp)
+
+    with app.app_context():
+        # Load model once
+        app.model = YOLO('models/aadhaar.pt')
+
     return app
