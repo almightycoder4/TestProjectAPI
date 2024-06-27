2b6ed3a10aac9839b40bc35c3cd88246f90d05aa
pawandev <pawan@aegiscovenant.com>
2024-06-27 13:39:19 +0530

    TEst versl 4

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
     from .api import ocr_bp
-    app.register_blueprint(ocr_bp, url_prefix='/')
-
+    app.register_blueprint(ocr_bp)
     return app
