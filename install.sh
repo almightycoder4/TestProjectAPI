dffd0eff94c2a9d428e03e29f23e33082158c501
pawandev <pawan@aegiscovenant.com>
2024-06-27 12:08:11 +0530

    Added Vercel support to deploy

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update package list and install dependencies
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr libgl1-mesa-glx python3 python3-pip
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install required Python packages
+pip3 install -r requirements.txt
+
+# Run the Flask application
+python3 run.py
