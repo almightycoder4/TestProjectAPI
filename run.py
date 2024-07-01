9626fa6d31da5415932ea86aa238aadfadee6fac
pawandev <pawan@aegiscovenant.com>
2024-07-01 11:04:35 +0530

    Fix some errors

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    if os.getenv("FLASK_ENV") != "production":
+    if os.getenv("FLASK_ENV") == "production":
         from gunicorn.app.wsgiapp import run
-        run(host='0.0.0.0', port=3000)
+        run()
     else:
         app.run(host='0.0.0.0', port=3000)
